aa2e66ac1d020f6db203dc575187bc2d9dea38ba
sertacturkel <msertacturkel@gmail.com>
2014-03-22 01:27:29 +0200

    admin paneli ile ilgili duzenleme yapildi.

--- whatisnext/src/users/models.py
+++ whatisnext/src/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.encoding import smart_unicode
 # Create your models here.
 class User(models.Model):
-    username=models.CharField(max_length=120,null=True,blank=True)
+    first_name=models.CharField(max_length=120,null=True,blank=True)
     last_name=models.CharField(max_length=120,null=True,blank=True)
     password = models.CharField(max_length=50,null=False,blank=False)
     location=models.CharField(max_length=120,null=True,blank=True)
