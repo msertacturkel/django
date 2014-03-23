76d93ac68461433dde0cc45ba019da712eda2ef3
sertacturkel <msertacturkel@gmail.com>
2014-03-23 12:22:55 +0200

    duzenleme yapildi.

--- whatsnext/src/whatsnext/email_info.py
+++
@@ -1,7 +0,0 @@
-DEFAULT_FROM_EMAIL = 'gakgun87@gmail.com'
-SERVER_EMAIL = 'msertacturkel@gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'msertacturkel@gmail.com'
-EMAIL_HOST_PASSWORD = 'deneme'
